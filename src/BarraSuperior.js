c10d442b597fc49bfc3cbd9065679f07a09205bd
Mary-Paz-AN <marypaz.an.0@gmail.com>
2024-09-27 23:25:55 -0600

    Cambios fot y add autos premium

--- src/BarraSuperior.js
+++ src/BarraSuperior.js
@@ -1,11 +1,23 @@
 import './BarraSuperior.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 function BarraSuperior() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 987);
+    };
+    handleResize(); 
+    window.addEventListener('resize', handleResize); 
+    return () => {
+      window.removeEventListener('resize', handleResize); 
+    };
+  }, []);
 
   const irInicio = () => {
     navigate('/');
@@ -13,6 +25,10 @@ function BarraSuperior() {
 
   const irAutosNuevos = () => {
     navigate('/AutosNuevos');
+  };
+
+  const irAutosPremium = () => {
+    navigate('/AutosPremium');
   };
 
   const irContactenos = () => {
@@ -30,9 +46,10 @@ function BarraSuperior() {
           <Nav className="ms-auto">
             <Nav.Link onClick={irAutosNuevos} className="nav-link">Autos Nuevos</Nav.Link>
             <Nav.Link href="#autos-usados" className="nav-link">Autos Usados</Nav.Link>
-            <Nav.Link href="#usados-premium" className="nav-link">Usados Premium</Nav.Link>
+            <Nav.Link onClick={irAutosPremium} href="#usados-premium" className="nav-link">Usados Premium</Nav.Link>
             <Nav.Link href="#libro-azul" className="nav-link">Libro Azul</Nav.Link>
             <Nav.Link href="#simular-financiamiento" className="nav-link">Simular Financiamiento</Nav.Link>
+            <p className="nav-link">{isSmallScreen ? '⎯' : '|'}</p> {/* Aquí cambia el símbolo */}
             <Nav.Link onClick={irContactenos} className="nav-link">Contáctenos</Nav.Link>
             <Nav.Link href="#registrarse" className="nav-link">Registrarse</Nav.Link>
             <Nav.Link href="#ingresar" className="nav-link">Ingresar</Nav.Link>
---
+++ src/autosPremium.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import './autoNuevoStyle.css';
+import './Logos.css';
+import FiltroAutosPremium from './FiltroAutosPremium';
+import BarraSuperior from './BarraSuperior';
+import BarraInferior from './BarraInferior';
+
+// Componente funcional de React para mostrar autos nuevos
+const AutosPremium = () => {
+
+
+  return (
+    <div className='scrollConteiner'>
+      <BarraSuperior/>
+      <div className='mainContainer'>
+        <div className='titleContainer'>
+          <h1 className='h1Style'>Autos Usados Premium</h1>
+        </div>
+        <h2 className='h2Style'>
+            En esta sección encontrará los vehículos de las marcas mundialmente reconocidas como <strong>PREMIUM</strong>, y que no tengan más de 5 años de antigüedad.
+            Explore diferentes modelos y elige el que mejor se adapte a sus expectativas.
+        </h2>
+
+        <div className='contentContainer'>
+          <FiltroAutosPremium />
+
+          <div className='marcaLogoContainer'>
+            <h3 className='title'>Marcas</h3>
+            <div className='logoContainer'>
+              {/*Audi */}
+              <div className='logo'>
+                <img src='/logosMarcas/audi.png' alt='Logo de Audi' className='logoImagen'/>
+                <h3 className='logoTitle'>Audi</h3>
+              </div>
+              {/*Bentley */}
+              <div className='logo'>
+                <img src='/logosMarcas/bentley.png' alt='Logo de Bentley' className='logoImagen'/>
+                <h3 className='logoTitle'>Bentley</h3>
+              </div>
+              {/*BMW */}
+              <div className='logo'>
+                <img src='/logosMarcas/bmw.png' alt='Logo de BMW' className='logoImagen'/>
+                <h3 className='logoTitle'>BMW</h3>
+              </div>
+              {/*Jaguar */}
+              <div className='logo'>
+                <img src='/logosMarcas/jaguar.png' alt='Logo de Jaguar' className='logoImagen'/>
+                <h3 className='logoTitle'>Jaguar</h3>
+              </div>
+              {/*Land Rover */}
+              <div className='logo'>
+                <img src='/logosMarcas/landRover.png' alt='Logo de Land Rover' className='logoImagen'/>
+                <h3 className='logoTitle'>Land Rover</h3>
+              </div>
+              {/*Lexus */}
+              <div className='logo'>
+                <img src='/logosMarcas/lexus.png' alt='Logo de Lexus' className='logoImagen'/>
+                <h3 className='logoTitle'>Lexus</h3>
+              </div>
+              {/*Maserati */}
+              <div className='logo'>
+                <img src='/logosMarcas/maserati.png' alt='Logo de Maserati' className='logoImagen'/>
+                <h3 className='logoTitle'>Maserati</h3>
+              </div>
+              {/*Mercedes Benz */}
+              <div className='logo'>
+                <img src='/logosMarcas/mercedesBenz.png' alt='Logo de Mercedes Benz' className='logoImagen'/>
+                <h3 className='logoTitle'>Mercedes Benz</h3>
+              </div>
+              {/*Mini Cooper */}
+              <div className='logo'>
+                <img src='/logosMarcas/miniCooper.png' alt='Logo de Mini Cooper' className='logoImagen'/>
+                <h3 className='logoTitle'>Mini Cooper</h3>
+              </div>
+              {/*Porsche */}
+              <div className='logo'>
+                <img src='/logosMarcas/porsche.png' alt='Logo de Porsche' className='logoImagen'/>
+                <h3 className='logoTitle'>Porsche</h3>
+              </div>
+              {/*Volvo */}
+              <div className='logo'>
+                <img src='/logosMarcas/volvo.png' alt='Logo de Volvo' className='logoImagen'/>
+                <h3 className='logoTitle'>Volvo</h3>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <BarraInferior/>
+    </div>
+  );
+};
+
+export default AutosPremium;
